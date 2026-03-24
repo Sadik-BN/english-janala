5f6f1b4fab2a341411502189d639a04298d0fa36
Sadik-BN <sadikonline4@gmail.com>
2026-03-24 20:04:59 +0600

    Some exception handled

--- scripts/index.js
+++ scripts/index.js
@@ -12,6 +12,17 @@ function loadLessons(level_no) {
 
 const displayLessons = (lesson) => {
     document.getElementById("lesson-section").innerHTML = "";
+    if (lesson.length == 0) {
+        const noCard = document.createElement("div");
+        noCard.classList.add("col-span-full");
+        noCard.innerHTML = `
+            <div class="flex justify-center"><img src="assets/alert-error.png" alt=""></div>
+            <p class="bangla-font text-[#79716B] text-[14px] text-center">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি।</p>
+            <h1 class="bangla-font text-center font-medium text-[35px]">নেক্সট Lesson এ যান</h1>
+        `
+        document.getElementById("lesson-section").appendChild(noCard);
+        return;
+    }
     for (let words of lesson) {
         let word = words.word;
         let meaning = words.meaning;
@@ -23,9 +34,9 @@ const displayLessons = (lesson) => {
 
         wordCard.innerHTML = `
             <div class="flex flex-col justify-center items-center">
-                <h1 class="font-bold text-[32px] text-center">${word}</h1>
+                <h1 class="font-bold text-[32px] text-center">${word == null ? "Not Found" : word}</h1>
                 <p class="font-medium text-[20px] text-center">Meaning /Pronounciation</p>
-                <h1 class="bangla-font font-semibold text-[32px] text-center">"${meaning} / ${pronounciation}"</h1>
+                <h1 class="bangla-font font-semibold text-[32px] text-center">"${meaning == null ? "Meaning Not Found" : meaning} / ${pronounciation == null ? "Pronounciation Not Found" : pronounciation}"</h1>
             </div>
             <div class="flex justify-between">
                 <button class="btn bg-[#1a90ff1e]"><i class="fa-solid fa-circle-info"
